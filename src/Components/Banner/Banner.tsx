447e56eb8c15a22b1b9f85f26486ed11fb1a20db
farukphero <farukk6319@gmail.com>
2023-09-28 22:23:18 +0600

    first commit

---
+++ src/Components/Banner/Banner.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel assumenda delectus soluta dolorum reprehenderit ullam in ex facere reiciendis magnam atque facilis a libero aliquid odio, nesciunt totam? Atque, expedita. Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis vero corrupti, tempora natus fugiat obcaecati magnam nihil modi accusamus rem enim hic optio debitis illo officiis, quasi maiores adipisci eius. Lorem ipsum dolor sit amet consectetur adipisicing elit. In voluptatibus aperiam ducimus magnam odio aliquid ad inventore aut necessitatibus dignissimos quam rerum mollitia facilis ipsa, asperiores officia provident, tempore eaque.
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/Components/Shared/SideBar/SideBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SideBar = () => {
+    return (
+        <div>
+             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae sunt, placeat asperiores illo provident in quam itaque earum eaque hic adipisci, aliquam, nesciunt accusamus natus necessitatibus architecto! Aliquid, architecto in.
+        </div>
+    );
+};
+
+export default SideBar; 
